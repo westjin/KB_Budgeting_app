23680f596a1fc3640628944dfbb09ce42347973e
Hyunji-JANG <78188222+Hyunji-JANG@users.noreply.github.com>
2025-04-08 14:18:39 +0900

    FEAT #2 프로파일 페이지 기본 템플릿 추가

---
+++ Budgeting-app/src/components/ProfileImage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex justify-center">
+    <img
+      src="../assets/icons/character.png"
+      alt="Profile"
+      class="w-24 h-24 rounded-full bg-gray-100"
+    />
+  </div>
+</template>
