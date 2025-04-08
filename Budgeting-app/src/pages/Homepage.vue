8626f17f5366d6f1f171eb02e2b96c64adfed837
westjin <uko802@naver.com>
2025-04-08 10:12:26 +0900

    DOCS 기본설정 수정

--- Budgeting-app/src/pages/Homepage.vue
+++ Budgeting-app/src/pages/Homepage.vue
@@ -1,5 +1,8 @@
-<script setup></script>
-
 <template>
-  <h1>HOMEPAGE</h1>
+  <div>
+    <div class="title">공통 스타일 확인</div>
+    <div class="card">이건 카드입니다</div>
+    <div class="select-box">선택 항목</div>
+    <button class="button">확인 버튼</button>
+  </div>
 </template>
