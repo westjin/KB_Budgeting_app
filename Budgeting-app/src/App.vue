9095b0cd16d72a06a2399bba18804599ae9ef373
Hyunji-JANG <78188222+Hyunji-JANG@users.noreply.github.com>
2025-04-08 13:10:02 +0900

    FIX merge

--- Budgeting-app/src/App.vue
+++ Budgeting-app/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import HomePage from './pages/Homepage.vue';
+import Profile from './pages/Profile.vue';
 </script>
 
 <template>
-  <HomePage />
+  <!-- <HomePage /> -->
+  <Profile />
 </template>
