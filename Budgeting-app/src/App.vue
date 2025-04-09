ca527329c1df373763f4f137fe973898224cf1f9
suminiee <chansu0114@naver.com>
2025-04-09 16:23:07 +0900

    [HOTFIX] ROUTE 경로, IMPORT 수정

--- Budgeting-app/src/App.vue
+++ Budgeting-app/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterView } from 'vue-router';
-import BottomNav from '@/components/BottomNav.vue';
 import HomePage from './pages/Homepage.vue';
 import Profile from './pages/Profile.vue';
 import TransactionAdd from './pages/TransactionAdd.vue';
