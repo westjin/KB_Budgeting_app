291fda6b19f2fcedccee707dc1f9296268a235c3
westjin <uko802@naver.com>
2025-04-10 11:41:36 +0900

    FEAT 국가 업데이트

--- Budgeting-app/vite.config.js
+++ Budgeting-app/vite.config.js
@@ -4,11 +4,11 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
-import tailwindcss from '@tailwindcss/vite';
+// import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
